75dd2728bac6e50be1beb69a794105b1d32ca53c
Óscar Criado de la Torre <ocriado91@gmail.com>
2024-12-02 10:21:47 +0000

    Split read puzzle input and extraction of location IDs by group functions

--- src/day1.py
+++ src/day1.py
@@ -1,15 +1,20 @@
 """Advent Of Code 2024 - Solution of Day 1."""
 
 
-def solution_day_one_problem_one(puzzle_input: str = "data/day1.dat") -> int:
-    """Solution of day 1."""
+def read_puzzle_input(puzzle_input: str) -> list:
+    """Read and preprocess puzzle input."""
     # Read all lines of puzzle input
     with open(puzzle_input, "r") as f:
         lines = f.readlines()
 
-    # Remove end of line character
+    # Remove end of line characters.
     lines = [line.replace("\n", "") for line in lines]
 
+    return lines
+
+
+def extract_location_ids(lines: list) -> tuple:
+    """Extract the location ID by groups."""
     # Split the location IDs of group 1 and group 2 and convert them
     # to integer
     data = [line.split("   ") for line in lines]
@@ -20,6 +25,16 @@ def solution_day_one_problem_one(puzzle_input: str = "data/day1.dat") -> int:
     # Sort location IDs
     loc_group1.sort()
     loc_group2.sort()
+
+    return loc_group1, loc_group2
+
+
+def solution_day_one_problem_one(puzzle_input: str = "data/day1.dat") -> int:
+    """Solution of first problem of day 1."""
+    lines = read_puzzle_input(puzzle_input)
+
+    # Extract location IDs by group
+    loc_group1, loc_group2 = extract_location_ids(lines)
 
     # Compute the distance of each pair of sorted location IDs of both groups
     # and return the sum of distances
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,6 +1,25 @@
 """Calculator package Unit tests."""
 
-from day1 import solution_day_one_problem_one
+from day1 import (
+    read_puzzle_input,
+    extract_location_ids,
+    solution_day_one_problem_one,
+)
+
+
+def test_read_puzzle_input() -> None:
+    """Check reading puzzle input behaviour."""
+    lines = read_puzzle_input("tests/data/example_day1.dat")
+    assert len(lines) == 6
+    assert lines[0] == "3   4"
+
+
+def test_extract_location_id() -> None:
+    """Check split the location ID by group."""
+    lines = ["3   4"]
+    loc_group1, loc_group2 = extract_location_ids(lines)
+    assert loc_group1[0] == 3
+    assert loc_group2[0] == 4
 
 
 def test_solution_day_one() -> None:
