653b4fcf7f8b421f7e9cf326f80043721ef1de91
Óscar Criado de la Torre <ocriado91@gmail.com>
2024-12-03 12:58:10 +0000

    Add auxiliar functions to determine the problem conditions

--- src/day2.py
+++ src/day2.py
@@ -8,9 +8,43 @@ def read_puzzle_input(puzzle_input: str) -> list:
         lines = f.readlines()
 
     # Remove end of line characters.
-    lines = [line.replace("\n", "") for line in lines]
+    reports = [line.replace("\n", "") for line in lines]
 
-    return lines
+    # Split reports into levels and convert them into integer.
+    _reports = [x.split() for x in reports]
+    reports = []
+    for report in _reports:
+        report = [int(level) for level in report]
+        reports.append(report)
+
+    return reports
+
+
+def _compute_differences(report: list) -> list:
+    """Compute the difference between levels."""
+    diffs = []
+    for idx in range(len(report) - 1):
+        diff = report[idx + 1] - report[idx]
+        diffs.append(diff)
+    return diffs
+
+
+def _check_all_increasing(diff: list) -> int:
+    """Check if difference elements are in increased order."""
+    count = [x > 0 for x in diff].count(True)
+    return count
+
+
+def _check_all_decreasing(diff: list) -> int:
+    """Check if all levels are in decreasing order."""
+    count = [x < 0 for x in diff].count(True)
+    return count
+
+
+def _check_adjacent_levels(diff: list) -> int:
+    """Check adjacent levels levels."""
+    count = [abs(x) <= 3 and abs(x) >= 1 for x in diff].count(True)
+    return count
 
 
 def solution_day_two_problem_one(puzzle_input: str = "data/day2.dat") -> int:
@@ -19,27 +53,18 @@ def solution_day_two_problem_one(puzzle_input: str = "data/day2.dat") -> int:
 
     safe_count = 0
     for report in reports:
-        # Extract levels and convert them to integers
-        levels = report.split()
-        levels = [int(level) for level in levels]
-
         # Compute the difference between two adjency items.
-        diffs = []
-        for idx in range(len(levels) - 1):
-            diff = levels[idx + 1] - levels[idx]
-            diffs.append(diff)
+        diffs = _compute_differences(report)
 
         # Check if all differences are positive or negative (first condition)
-        all_increasing = all(diff >= 0 for diff in diffs)
-        all_decreasing = all(diff <= 0 for diff in diffs)
+        all_increasing = _check_all_increasing(diffs) == len(diffs)
+        all_decreasing = _check_all_decreasing(diffs) == len(diffs)
 
         if not all_increasing and not all_decreasing:
             continue
 
         # If first condition is meet, check the second condition
-        second_condition = all(
-            abs(diff) <= 3 and abs(diff) >= 1 for diff in diffs
-        )
+        second_condition = _check_adjacent_levels(diffs) == len(diffs)
         if not second_condition:
             continue
 
--- tests/test_day2.py
+++ tests/test_day2.py
@@ -1,6 +1,64 @@
 """Unit Test for AoC Day 2."""
 
-from day2 import solution_day_two_problem_one
+from day2 import (
+    read_puzzle_input,
+    _compute_differences,
+    _check_all_increasing,
+    _check_all_decreasing,
+    _check_adjacent_levels,
+    solution_day_two_problem_one,
+)
+
+
+def test_read_puzzle_input() -> None:
+    """Check the reading puzzle input."""
+    levels = read_puzzle_input("tests/data/example_day2.dat")
+    assert len(levels) == 6
+    assert levels[0] == [7, 6, 4, 2, 1]
+
+
+def test_compute_differences() -> None:
+    """Chech the computation of difference between adjenct levels."""
+    levels = read_puzzle_input("tests/data/example_day2.dat")
+    diffs = _compute_differences(levels[0])
+    assert len(diffs) == 4
+    assert diffs == [-1, -2, -2, -1]
+
+
+def test_check_all_increasing() -> None:
+    """Check all increasing reports into incoming data."""
+    count = 0
+    reports = read_puzzle_input("tests/data/example_day2.dat")
+    for report in reports:
+        diffs = _compute_differences(report)
+        increasing_values = _check_all_increasing(diffs)
+        if increasing_values == len(diffs):
+            count += 1
+    assert count == 2
+
+
+def test_check_all_decreasing() -> None:
+    """Check all decreasing reports into incoming data."""
+    count = 0
+    reports = read_puzzle_input("tests/data/example_day2.dat")
+    for report in reports:
+        diffs = _compute_differences(report)
+        decreasing_values = _check_all_decreasing(diffs)
+        if decreasing_values == len(diffs):
+            count += 1
+    assert count == 2
+
+
+def test_check_adjacent_levels() -> None:
+    """Check adjacent difference threshold into incoming data."""
+    count = 0
+    reports = read_puzzle_input("tests/data/example_day2.dat")
+    for report in reports:
+        diffs = _compute_differences(report)
+        adj_values = _check_adjacent_levels(diffs)
+        if adj_values == len(diffs):
+            count += 1
+    assert count == 3
 
 
 def test_solution_day_two_problem_one() -> None:
