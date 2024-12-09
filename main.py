3fcc4a7b386ca1e29cf2a838dbf844b599a1508d
Óscar Criado de la Torre <ocriado91@gmail.com>
2024-12-09 12:57:55 +0000

    Add AoC Day 5 - Problem 2 solution

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from day1 import solution_day_one_problem_one, solution_day_one_problem_two
 from day2 import solution_day_two_problem_one, solution_day_two_problem_two
 from day3 import solution_day_three_problem_one, solution_day_three_problem_two
 from day4 import solution_day_four_problem_one, solution_day_four_problem_two
+from day5 import solution_day_five_problem_one, solution_day_five_problem_two
 
 if __name__ == "__main__":
     # Day One - Problem 1
@@ -24,4 +25,9 @@ if __name__ == "__main__":
     # Day Four - Problem 1
     print("Problem 1 - Day 4 Solution:", solution_day_four_problem_one())
     # Day Four - Problem 2
-    print("Problem 2 - DAy 4 Solution:", solution_day_four_problem_two())
+    print("Problem 2 - Day 4 Solution:", solution_day_four_problem_two())
+
+    # Day Five - Problem 1
+    print("Problem 1 - Day 5 Solution:", solution_day_five_problem_one())
+    # Day Five - Problem 2
+    print("Problem 2 - Day 5 Solution:", solution_day_five_problem_two())
--- src/day5.py
+++ src/day5.py
@@ -1,5 +1,6 @@
 """Advent Of Code 2024 - Solution of Day 5."""
 
+import copy
 import math
 import re
 
@@ -41,6 +42,7 @@ def _split_ordering_updates(lines: list) -> tuple:
 def _check_updates(ordering_rules: list, updates: list) -> list:
     """Check the validity of the updates according to the ordering rules."""
     valid_updates = []
+    invalid_updates = []
     # Check page by page of each update, if there is any page that must be
     # printed before of current page and if these pages are into next pages
     # positions within the update.
@@ -52,12 +54,43 @@ def _check_updates(ordering_rules: list, updates: list) -> list:
                 for x, y in ordering_rules
                 if y == page and x in update[idx + 1 :]
             ]
+            # Detected a page into the update that breaks the order. Set the
+            # invalid update flag as True and stop to search into the next
+            # pages
             if before_pages:
                 invalid_update = True
+                invalid_updates.append(update)
                 break
         if not invalid_update:
             valid_updates.append(update)
-    return valid_updates
+    return valid_updates, invalid_updates
+
+
+def fix_updates(invalid_updates: list, ordering_rules: list) -> list:
+    """Fix update according to ordering rules."""
+    fixed_updates = []
+    for invalid_update in invalid_updates:
+        for idx, page in enumerate(invalid_update):
+            previous_pages = [
+                x
+                for x, y in ordering_rules
+                if y == page and x in invalid_update[idx + 1 :]
+            ]
+            if previous_pages:
+                # Check the indexes of previous pages and corrupted page
+                replace_index = max(
+                    [invalid_update.index(x) for x in previous_pages]
+                )
+                page_index = invalid_update.index(page)
+                # Replace the corrupt page with the last index
+                fixed_update = copy.deepcopy(invalid_update)
+                fixed_update[replace_index] = page
+                fixed_update[page_index] = invalid_update[replace_index]
+                # Add current fixed update to list
+                fixed_updates.append(fixed_update)
+                # Detected one corrupted page. Stop the current search loop
+                break
+    return fixed_updates
 
 
 def compute_middle_page_number(update: list) -> int:
@@ -72,7 +105,7 @@ def solution_day_five_problem_one(puzzle_input: str = "data/day5.dat") -> int:
     # Divide the lines of puzzle input into ordering rules and updates
     ordering_rules, updates = _split_ordering_updates(lines)
     # Check the validity of update list.
-    valid_updates = _check_updates(ordering_rules, updates)
+    valid_updates, _ = _check_updates(ordering_rules, updates)
     # Add the middle page numbers of valid pages.
     sum = 0
     for valid_update in valid_updates:
@@ -80,6 +113,36 @@ def solution_day_five_problem_one(puzzle_input: str = "data/day5.dat") -> int:
     return sum
 
 
+def solution_day_five_problem_two(puzzle_input: str = "data/day5.dat") -> int:
+    """Solution of the second problem of Aoc Day 5."""
+    lines = read_puzzle_input(puzzle_input)
+    # Divide the lines of puzzle input into ordering rules and updates
+    ordering_rules, updates = _split_ordering_updates(lines)
+    # Check the validity of update list.
+    _, invalid_updates = _check_updates(ordering_rules, updates)
+    # Try to fix invalid updates until list is empty (all invalid updates has
+    # been fixed)
+    valid_fixed_updates = []
+    while invalid_updates:
+        # Fix invalid updates
+        fixed_updates = fix_updates(invalid_updates, ordering_rules)
+        # Check the validity of fixed updates
+        _valid_fixed_updates, invalid_updates = _check_updates(
+            ordering_rules, fixed_updates
+        )
+        # Save valid fixed updates into list.
+        for _valid_fixed_update in _valid_fixed_updates:
+            valid_fixed_updates.append(_valid_fixed_update)
+
+    # Compute the sum of all middle page numbers of fixed updates.
+    sum = 0
+    for valid_update in valid_fixed_updates:
+        sum += compute_middle_page_number(valid_update)
+    return sum
+
+
 if __name__ == "__main__":
     solution1 = solution_day_five_problem_one()
     print(solution1)
+    solution2 = solution_day_five_problem_two()
+    print(solution2)
