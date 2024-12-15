843fdbaec4027e5e428ef72723995dd4ab065a79
Óscar Criado de la Torre <ocriado91@gmail.com>
2024-12-15 17:35:17 +0000

    Implementation of first problem AoC - Day 7

---
+++ tests/test_day7.py
@@ -0,0 +1,10 @@
+"""Unit Test for AoC Day 7."""
+
+from day7 import Day7
+
+
+def test_solution_day_seven_first_problem():
+    """Check the example solution of first problem of AoC - Day 7."""
+    day7 = Day7("tests/data/example_day7.dat")
+    solution = day7.solution_problem_one()
+    assert solution == 3749
