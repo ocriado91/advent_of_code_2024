5e18dcdb8e7c9f3ab5b75c537eebf15edf26e8b7
Óscar Criado de la Torre <ocriado91@gmail.com>
2024-12-15 22:21:37 +0000

    Restore expected output of example of the first problem of AoC Day 7

--- tests/test_day7.py
+++ tests/test_day7.py
@@ -7,7 +7,7 @@ def test_solution_day_seven_first_problem():
     """Check the example solution of first problem of AoC - Day 7."""
     day7 = Day7("tests/data/example_day7.dat")
     solution = day7.solution_problem_one()
-    assert solution == 0
+    assert solution == 3749
 
 
 def test_solution_day_seven_second_problem():
