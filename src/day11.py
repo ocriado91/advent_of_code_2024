b771eec7711fe346d1959f116ae44f1f99de5a8f
Óscar Criado de la Torre <ocriado91@gmail.com>
2024-12-22 11:59:38 +0000

    Use of defaultdict data structure to store the count of all digits and avoid cpu and memory consumption issues

--- src/day11.py
+++ src/day11.py
@@ -1,5 +1,6 @@
 """Advent Of Code 2024 - Solution of Day 11."""
 
+from collections import defaultdict
 from solution import Solution
 
 
@@ -11,46 +12,53 @@ class Day11(Solution):
         Solution.__init__(self, puzzle_input)
 
         # Store the initial stones digits
-        self.stones = self.lines[0].split()
+        self.stones = defaultdict(int)
+        for stone in self.lines[0].split():
+            self.stones[int(stone)] += 1
 
-    def solution_problem_one(self) -> int:
-        """Solution of the first problem of AoC - Day 11."""
+    def stone_transitions(self, blinking_end: int) -> int:
+        """Compute the stone changes between each eye blink."""
         blinking_times = 0
-        blinking_end = 25
         while blinking_end > blinking_times:
-            stones = []
+            stones = defaultdict(int)
             for stone in self.stones:
                 # Apply first rule -> If the stone is engraved with the number
                 # 0, it is replaced by a stone engraved with the number 1.
-                if stone == "0":
-                    stone = "1"
-                    stones.append(stone)
+                if stone == 0:
+                    stones[1] += self.stones[0]
                 # Apply second rule -> If the stone is engraved with a number
                 # that has an even number of digits, it is replaced by two
                 # stones. The left half of the digits are engraved on the new
                 # left stone,and the right half of the digits are engraved on
                 # the new right stone. (The new numbers don't keep extra
                 # leading zeroes: 1000 would become stones 10 and 0.)
-                elif len(stone) % 2 == 0:
-                    half_position = int(len(stone) / 2)
-                    first_half_stone = str(int(stone[:half_position]))
-                    second_half_stone = str(int(stone[half_position:]))
-                    stones.append(first_half_stone)
-                    stones.append(second_half_stone)
+                elif len(str(stone)) % 2 == 0:
+                    half_position = int(len(str(stone)) / 2)
+                    first_half_stone = int(str(stone)[:half_position])
+                    second_half_stone = int(str(stone)[half_position:])
+                    stones[first_half_stone] += self.stones[int(stone)]
+                    stones[second_half_stone] += self.stones[int(stone)]
                 # Apply third rule -> If none of the other rules apply, the
                 # stone is replaced by a new stone; the old stone's number
                 # multiplied by 2024 is engraved on the new stone.
                 else:
-                    stone = str(int(stone) * 2024)
-                    stones.append(stone)
+                    stones[int(stone) * 2024] += self.stones[int(stone)]
             blinking_times += 1
             # Update original stones
             self.stones = stones
-        return len(self.stones)
+
+        return stones
+
+    def solution_problem_one(self) -> int:
+        """Solution of the first problem of AoC - Day 11."""
+        stones = self.stone_transitions(blinking_end=25)
+        return sum(stones.values())
 
     def solution_problem_two(self) -> int:
         """Solution of the second problem of AoC - Day 11."""
-        pass
+        stones = self.stone_transitions(blinking_end=75)
+
+        return sum(stones.values())
 
 
 if __name__ == "__main__":
--- tests/test_day11.py
+++ tests/test_day11.py
@@ -8,3 +8,10 @@ def test_solution_day_eleven_first_problem():
     day11 = Day11("tests/data/example_day11.dat")
     solution = day11.solution_problem_one()
     assert solution == 55312
+
+
+def test_solution_day_eleven_second_problem():
+    """Check the example solution of second problem of AoC Day 11."""
+    day11 = Day11("tests/data/example_day11.dat")
+    solution = day11.solution_problem_two()
+    assert solution == 65601038650482
